3e6c1ab743efe22d702b8e58c1d00c3371bc9f5e
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-03-05 13:41:53 -0800

    Skeleton BookingSystem

---
+++ specs/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'date'
+require "simplecov"
+SimpleCov.start
+
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+# Add simplecov
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+# Require_relative your lib files here!
+require_relative '../lib/administrator'
+require_relative '../lib/reservation'
+require_relative '../lib/room'
